d53602ddfe3757817afc59589edbefa7d273d15d
chenjinge <chenjinge@didichuxing.com>
2018-11-24 16:27:37 +0800

    请求数据接口100%

---
+++ data.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    name: "todo1",
+    createAt: "2013-03-07T07:00:00-08:00"
+  },
+  {
+    name: "todo2",
+    createAt: "2018-03-07T07:00:00-08:00"
+  },
+  {
+    name: "todo3",
+    createAt: "2020-03-07T07:00:00-08:00"
+  },
+];
+
+module.exports = data;
--- demo.js
+++ demo.js
@@ -1,30 +1,26 @@
+const data = require('./data');
+const moment = require('moment');
 const Koa = require('koa');
 const app = new Koa();
-app.use(async(ctx)=>{
-    // let url =ctx.url;
-    // let request =ctx.request;
-    // let req_query = request.query;
-    // let req_querystring = request.querystring;
-    ctx.body={
-        // url,
-        // req_query,
-        // req_querystring
-        data:[
-          {
-            name:"todo1",
-            createAt:new Date().getTime()
-          },
-          {
-            name:"todo2",
-            createAt:new Date().getTime()
-          },
-          {
-            name:"todo3",
-            createAt:new Date().getTime()
-          },
-        ]
+
+app.use(async (ctx) => {
+  let url = ctx.url;
+  let request = ctx.request;
+  let userTime = request.query.time;
+
+  let dataFilter = data.map((item) => {
+    if (Date.parse(item.createAt) > Date.parse(new Date(userTime))) {
+      return moment(item.createAt).format();
     }
+  });
+
+  ctx.body = {
+    errno:0,
+    datamsg:"请求成功",
+    data:dataFilter,
+  }
+
 });
-app.listen(3000,()=>{
-    console.log('[demo] server is starting at port 3000');
+app.listen(3000, () => {
+  console.log('[demo] server is starting at port 3000');
 });
